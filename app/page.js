f644da98104406f6052a03d5473325d5ecbf8d38
NoOnE8151 <lomash265@gmail.com>
2025-07-02 11:56:33 +0530

    intial setup and created landing page UI

--- app/page.js
+++ app/page.js
@@ -1,103 +1,34 @@
-import Image from "next/image";
+import { Menu } from 'lucide-react';
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm/6 text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-[family-name:var(--font-geist-mono)] font-semibold">
-              app/page.js
-            </code>
-            .
-          </li>
-          <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
-          </li>
-        </ol>
+   <div className="w-[100vw] h-[100vh] overflow-hidden bg-background">
+    <header className="flex items-center justify-between px-5 py-3 shadow-2xl h-[10vh]">
+      <div className="flex items-center gap-3">
+        <img src="/assets/logo/logo.png" alt="logo" className="w-[3rem]" />
+        <h1 className="font-bold text-xl ">Digimart</h1>
+      </div>
+      <nav className="hidden lg:flex items-center gap-5 font-semibold">
+        <div>Features</div>
+        <div>Sell</div>
+        <div>Pricing</div>
+      </nav>
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
+      <div className="lg:flex hidden items-center gap-5 w-[15%]">
+        <button className="bg-[#ECECEC] hover:bg-[#e1e1e1] active:bg-[#eeeeee] px-5 py-2 w-1/2 rounded-full font-semibold shadow cursor-pointer">Login</button>
+        <button className="bg-[#3B82F6] hover:bg-[#3675db] active:bg-[#297bff] text-white px-5 py-2 w-1/2 rounded-full font-semibold shadow cursor-pointer ">Sign&nbsp;Up</button>
+      </div>
+
+      <button className='lg:hidden'><Menu /></button>
+    </header>
+
+    <main className="flex flex-col items-center lg:justify-center justify-start relative top-[20vh] lg:top-0 lg:h-[90vh] h-[97vh] px-3">
+      <div className="flex flex-col lg:gap-7 gap-5 items-center justify-center lg:w-[50vw] md:w-[70vw] text-center">
+      <h1 className="lg:text-6xl text-3xl md:text-5xl font-bold">Turn your creativity into income in minutes.</h1>
+      <p className="lg:text-3xl text-lg md:text-2xl text-[#575757] font-semibold">Sell with UPI, get paid instantly</p>
+      <button className="bg-[#3B82F6] hover:bg-[#3675db] active:bg-[#297bff] cursor-pointer text-white px-5 py-3 rounded-full lg:text-2xl text-xl lg:w-1/2 md:w-1/2 font-semibold my-3">Start selling for free</button>
+      </div>
+    </main>
+   </div>
   );
 }
