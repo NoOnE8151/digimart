bd0dfb2899bc140bcba5cdf6cae8bd30a9c12c2b
NoOnE8151 <lomash265@gmail.com>
2025-07-03 13:32:31 +0530

    fixed shadcn and tailwind conflict

--- app/page.js
+++ app/page.js
@@ -1,10 +1,9 @@
 import { Menu } from 'lucide-react';
 import { SignInButton, SignOutButton, SignUpButton, SignedIn, SignedOut, UserButton } from "@clerk/nextjs"
-import { Button } from '@/components/ui/button';
 
 export default function Home() {
   return (
-   <div className="w-[100vw] h-[100vh] overflow-hidden bg-background">
+   <div className="w-[100vw] h-[100vh] overflow-hidden bg-background ">
     <header className="flex items-center justify-between px-5 py-3 shadow-2xl h-[10vh]">
       <div className="flex items-center gap-3">
         <img src="/assets/logo/logo.png" alt="logo" className="w-[3rem]" />
@@ -30,14 +29,13 @@ export default function Home() {
       <button className='lg:hidden'><Menu /></button>
     </header>
 
-    <main className="flex flex-col items-center lg:justify-center justify-start relative top-[20vh] lg:top-0 lg:h-[90vh] h-[97vh] px-3">
+    <main className="flex flex-col items-center lg:justify-center justify-start relative top-[20vh] lg:top-0 lg:h-[90vh] h-[97vh] px-3 ">
       <div className="flex flex-col lg:gap-7 gap-5 items-center justify-center lg:w-[50vw] md:w-[70vw] text-center">
       <h1 className="lg:text-6xl text-3xl md:text-5xl font-bold">Turn your creativity into income in minutes.</h1>
       <p className="lg:text-3xl text-lg md:text-2xl text-[#575757] font-semibold">Sell with UPI, get paid instantly</p>
      <SignUpButton> 
       <button className="bg-[#3B82F6] hover:bg-[#3675db] active:bg-[#297bff] cursor-pointer text-white px-5 py-3 rounded-full lg:text-2xl text-xl lg:w-1/2 md:w-1/2 font-semibold my-3">Start selling for free</button>
       </SignUpButton>
-      <Button>hllo</Button>
       </div>
     </main>
    </div>
